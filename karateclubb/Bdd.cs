0cea58082b30d43774942d9c04f42b9630e05ae7
Axel FORT <fort.axel@educ-valadon-limoges.fr>
2024-01-31 11:09:07 +0100

    2

--- karateclubb/Bdd.cs
+++ karateclubb/Bdd.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Windows.Forms;
 using karateclubb;
 using MySql.Data.MySqlClient;
@@ -6,11 +7,6 @@ using MySql.Data.MySqlClient;
 public class Bdd
 {
     private string connectionString = "Server=localhost;Database=karate;Uid=root;Pwd=;";
-
-    public Bdd()
-    {
-        
-    }
 
     public MySqlConnection OpenConnection()
     {
@@ -98,47 +94,231 @@ public class Bdd
         }
     }
 
-    public void FillDataGridViewWithMembres(ComboBox comboBox)
+    public DataTable GetMembresDataTable()
     {
         using (var connection = OpenConnection())
         {
             if (connection != null)
             {
                 var command = new MySqlCommand("SELECT num_licence, nom_membre, prenom_membre FROM membre", connection);
-                var reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    comboBox.Items.Add(new
-                    {
-                        Id = reader.GetInt32("num_licence"),
-                        FullName = $"{reader.GetString("nom_membre")} {reader.GetString("prenom_membre")}"
-                    });
-                    comboBox.DisplayMember = "NomComplet";
-                    comboBox.ValueMember = "NumLicence";
-                }
+                var adapter = new MySqlDataAdapter(command);
+                var dt = new DataTable();
+                adapter.Fill(dt);
+                return dt;
             }
+            return null;
         }
     }
 
-    public void FillDataGridViewWithCompetitions(ComboBox comboBox)
+    public DataTable GetCompetitionsDataTable()
     {
         using (var connection = OpenConnection())
         {
             if (connection != null)
             {
-                var command = new MySqlCommand("SELECT num_competition, num_club FROM competition", connection);
-                var reader = command.ExecuteReader();
-                while (reader.Read())
+                var command = new MySqlCommand(
+                    "SELECT c.num_competition, c.date_competition, cl.nom_club " +
+                    "FROM competition c " +
+                    "JOIN club cl ON c.num_club = cl.num_club", connection);
+                var adapter = new MySqlDataAdapter(command);
+                var dt = new DataTable();
+                adapter.Fill(dt);
+                return dt;
+            }
+            return null;
+        }
+    }
+
+    public void FillDataGridViewWithMembres(DataGridView dataGridView)
+    {
+        using (var connection = OpenConnection())
+        {
+            if (connection != null)
+            {
+                var command = new MySqlCommand("SELECT num_licence, nom_membre, prenom_membre FROM membre", connection);
+                var adapter = new MySqlDataAdapter(command);
+                var dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView.DataSource = dataTable;
+
+                dataGridView.Columns["num_licence"].HeaderText = "Num Licence";
+                dataGridView.Columns["nom_membre"].HeaderText = "Nom";
+                dataGridView.Columns["prenom_membre"].HeaderText = "Prénom";
+            }
+        }
+    }
+
+    public void FillDataGridViewWithCompetitions(DataGridView dataGridView)
+    {
+        using (var connection = OpenConnection())
+        {
+            if (connection != null)
+            {
+                var command = new MySqlCommand("SELECT num_competition, nom FROM competition", connection);
+                var adapter = new MySqlDataAdapter(command);
+                var dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView.DataSource = dataTable;
+
+                dataGridView.Columns["num_competition"].HeaderText = "Numéro de la compétition";
+                dataGridView.Columns["date_competition"].HeaderText = "Date de la compétition";
+                dataGridView.Columns["nom_club"].HeaderText = "Nom du club organisateur";
+
+            }
+        }
+    }
+
+    public bool AddInscription(int numCompetition, int numLicence)
+    {
+        using (MySqlConnection connection = OpenConnection())
+        {
+            if (connection == null) return false;
+
+            string query = @"INSERT INTO inscription (num_competition, num_licence) VALUES (@NumCompetition, @NumLicence)";
+            using (MySqlCommand command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@NumCompetition", numCompetition);
+                command.Parameters.AddWithValue("@NumLicence", numLicence);
+
+                try
                 {
-                    comboBox.Items.Add(new
-                    {
-                        Id = reader.GetInt32("num_competition"),
-                        Name = reader.GetInt32("num_club")
-                    });
-                    comboBox.DisplayMember = "Nom";
-                    comboBox.ValueMember = "NumLicence";
+                    int result = command.ExecuteNonQuery();
+                    return result > 0;
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show($"Une erreur est survenue : {ex.Message}", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
                 }
             }
         }
     }
+
+    public bool UpdateMembre(int numLicence, string nomMembre, string prenomMembre)
+    {
+        using (var connection = OpenConnection())
+        {
+            if (connection == null) return false;
+
+            string query = "UPDATE membre SET nom_membre = @NomMembre, prenom_membre = @PrenomMembre WHERE num_licence = @NumLicence";
+
+            using (var command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@NomMembre", nomMembre);
+                command.Parameters.AddWithValue("@PrenomMembre", prenomMembre);
+                command.Parameters.AddWithValue("@NumLicence", numLicence);
+
+                try
+                {
+                    int result = command.ExecuteNonQuery();
+                    if (result > 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("La mise à jour du membre a échoué.", "Erreur de Mise à Jour", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return false;
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show($"Une erreur s'est produite lors de la mise à jour du membre: {ex.Message}", "Erreur de Mise à Jour", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+            }
+        }
+    }
+
+
+    public bool DeleteMembre(int numLicence)
+    {
+        using (var connection = OpenConnection())
+        {
+            if (connection == null) return false;
+
+            string query = "DELETE FROM membre WHERE num_licence = @NumLicence";
+
+            using (var command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@NumLicence", numLicence);
+
+                try
+                {
+                    int result = command.ExecuteNonQuery();
+                    if (result > 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("La suppression du membre a échoué.", "Erreur de Suppression", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return false;
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show($"Une erreur s'est produite lors de la suppression du membre: {ex.Message}", "Erreur de Suppression", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+            }
+        }
+    }
+
+
+    public bool UpdateCompetition(int numCompetition, string nomCompetition, DateTime dateCompetition, int numClub)
+    {
+        using (var connection = OpenConnection())
+        {
+            if (connection == null) return false;
+
+            string query = "UPDATE competition SET nom_competition = @NomCompetition, date_competition = @DateCompetition WHERE num_competition = @NumCompetition";
+
+            using (var command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@NomCompetition", nomCompetition);
+                command.Parameters.AddWithValue("@DateCompetition", dateCompetition);
+                command.Parameters.AddWithValue("@NumCompetition", numCompetition);
+
+                try
+                {
+                    int result = command.ExecuteNonQuery();
+                    return result > 0;
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show($"Une erreur s'est produite lors de la mise à jour de la compétition: {ex.Message}", "Erreur de Mise à Jour", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+            }
+        }
+    }
+
+
+    public bool DeleteCompetition(int numCompetition)
+    {
+        using (var connection = OpenConnection())
+        {
+            if (connection == null) return false;
+
+            string query = "DELETE FROM competition WHERE num_competition = @NumCompetition";
+
+            using (var command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@NumCompetition", numCompetition);
+
+                try
+                {
+                    int result = command.ExecuteNonQuery();
+                    return result > 0;
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show($"Une erreur s'est produite lors de la suppression de la compétition: {ex.Message}", "Erreur de Suppression", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+            }
+        }
+    }
+
 }
--- karateclubb/InscriptionCompetitionForm.cs
+++ karateclubb/InscriptionCompetitionForm.cs
@@ -11,6 +11,7 @@ namespace karateclubb
         private Button inscrireButton = new Button();
         private Button nouveauMembreButton = new Button();
         private Button annulerButton = new Button();
+        Button refreshButton = new Button();
 
         private Bdd bdd = new Bdd();
 
@@ -18,20 +19,27 @@ namespace karateclubb
         {
             InitializeComponent();
 
+            refreshButton.Text = "Rafraîchir";
+            refreshButton.Size = new Size(100, 30);
+            refreshButton.Location = new Point(650, 500);
+            refreshButton.BackColor = Color.LightSkyBlue;
+            refreshButton.Click += RefreshButton_Click;
+            this.Controls.Add(refreshButton);
+
             this.BackColor = Color.MintCream;
-            this.Size = new Size(800, 600);
+            this.Size = new Size(800, 600); 
             this.Text = "INSCRIPTION À UNE COMPÉTITION";
 
-            ConfigurerDataGridView(membresDataGridView, 50, 40);
-            ConfigurerDataGridView(competitionsDataGridView, 450, 40);
+            ConfigurerDataGridView(membresDataGridView, 50, 40, 300, 200);
+            ConfigurerDataGridView(competitionsDataGridView, 400, 40, 300, 200);
 
-            ConfigurerButton(inscrireButton, "INSCRIRE", 50, 520);
+            ConfigurerButton(inscrireButton, "INSCRIRE", 50, 250);
             inscrireButton.Click += InscrireButton_Click;
 
-            ConfigurerButton(nouveauMembreButton, "NOUVEAU MEMBRE", 250, 520);
+            ConfigurerButton(nouveauMembreButton, "NOUVEAU MEMBRE", 150, 250);
             nouveauMembreButton.Click += NouveauMembreButton_Click;
 
-            ConfigurerButton(annulerButton, "ANNULER", 450, 520);
+            ConfigurerButton(annulerButton, "ANNULER", 250, 250);
             annulerButton.Click += (sender, e) => this.Close();
 
             this.Controls.Add(membresDataGridView);
@@ -40,20 +48,55 @@ namespace karateclubb
             this.Controls.Add(nouveauMembreButton);
             this.Controls.Add(annulerButton);
 
-            bdd.FillDataGridViewWithMembres(membresDataGridView);
-            bdd.FillDataGridViewWithCompetitions(competitionsDataGridView);
+            LoadMembres();
+            LoadCompetitions();
         }
 
-        private void ConfigurerDataGridView(DataGridView dataGridView, int x, int y)
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            LoadMembres();
+            LoadCompetitions();
+            MessageBox.Show("Les données ont été rafraîchies.");
+        }
+
+        private void LoadMembres()
+        {
+            membresDataGridView.DataSource = null; 
+            membresDataGridView.DataSource = bdd.GetMembresDataTable(); 
+        }
+
+        private void LoadCompetitions()
+        {
+            competitionsDataGridView.DataSource = null; 
+            competitionsDataGridView.DataSource = bdd.GetCompetitionsDataTable(); 
+        }
+
+        private void ConfigurerDataGridView(DataGridView dataGridView, int x, int y, int width, int height)
         {
             dataGridView.Location = new Point(x, y);
-            dataGridView.Size = new Size(300, 400);
+            dataGridView.Size = new Size(width, height);
+            dataGridView.ReadOnly = false;
+            dataGridView.AllowUserToAddRows = false; 
+            dataGridView.AllowUserToDeleteRows = true; 
+            dataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            if (dataGridView.Columns["deleteColumn"] == null)
+            {
+                var deleteButtonColumn = new DataGridViewButtonColumn
+                {
+                    Name = "deleteColumn",
+                    Text = "Supprimer",
+                    UseColumnTextForButtonValue = true,
+                    HeaderText = "Actions"
+                };
+                dataGridView.Columns.Add(deleteButtonColumn);
+            }
         }
 
         private void ConfigurerButton(Button button, string texte, int x, int y)
         {
             button.Text = texte;
-            button.Size = new Size(150, 40);
+            button.Size = new Size(80, 30);
             button.Location = new Point(x, y);
             button.BackColor = Color.LightSkyBlue;
             button.FlatStyle = FlatStyle.Flat;
@@ -61,8 +104,107 @@ namespace karateclubb
 
         private void InscrireButton_Click(object sender, EventArgs e)
         {
-            
+            if (membresDataGridView.SelectedRows.Count > 0 && competitionsDataGridView.SelectedRows.Count > 0)
+            {
+                var selectedMembreRow = membresDataGridView.SelectedRows[0];
+                int numLicence = Convert.ToInt32(selectedMembreRow.Cells["num_licence"].Value);
+
+                var selectedCompetitionRow = competitionsDataGridView.SelectedRows[0];
+                int numCompetition = Convert.ToInt32(selectedCompetitionRow.Cells["num_competition"].Value);
+
+                bool success = bdd.AddInscription(numCompetition, numLicence);
+
+                if (success)
+                {
+                    MessageBox.Show("Inscription réussie.");
+                }
+                else
+                {
+                    MessageBox.Show("L'inscription a échoué.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Veuillez sélectionner un membre et une compétition.");
+            }
         }
+
+
+
+        private void DataGridView_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+
+            membresDataGridView.CellValueChanged += DataGridView_CellValueChanged;
+            competitionsDataGridView.CellValueChanged += DataGridView_CellValueChanged;
+
+            if (e.RowIndex >= 0)
+            {
+                DataGridView dataGridView = sender as DataGridView;
+                if (dataGridView != null)
+                {
+                    int id = Convert.ToInt32(dataGridView.Rows[e.RowIndex].Cells["num_licence"].Value);
+                    string columnName = dataGridView.Columns[e.ColumnIndex].Name;
+                    object newValue = dataGridView.Rows[e.RowIndex].Cells[e.ColumnIndex].Value;
+
+                    if (dataGridView == membresDataGridView && (columnName == "nom_membre" || columnName == "prenom_membre"))
+                    {
+                        string nomMembre = dataGridView.Rows[e.RowIndex].Cells["nom_membre"].Value.ToString();
+                        string prenomMembre = dataGridView.Rows[e.RowIndex].Cells["prenom_membre"].Value.ToString();
+                        bdd.UpdateMembre(id, nomMembre, prenomMembre);
+                    }
+                    else if (dataGridView == competitionsDataGridView && (columnName == "nom_competition" || columnName == "date_competition"))
+                    {
+                        string nomCompetition = dataGridView["nom_competition", e.RowIndex].Value.ToString();
+                        DateTime dateCompetition = Convert.ToDateTime(dataGridView["date_competition", e.RowIndex].Value);
+                        int numClub = Convert.ToInt32(dataGridView["num_club", e.RowIndex].Value); 
+                        bdd.UpdateCompetition(id, nomCompetition, dateCompetition, numClub);
+                    }
+                }
+            }
+        }
+
+
+
+
+        private void DataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex == membresDataGridView.Columns["deleteColumn"].Index)
+            {
+                if (MessageBox.Show("Voulez-vous vraiment supprimer cet élément ?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    DataGridView dataGridView = sender as DataGridView;
+                    int id;
+                    bool success;
+
+                    if (dataGridView == membresDataGridView)
+                    {
+                        id = Convert.ToInt32(dataGridView["num_licence", e.RowIndex].Value);
+                        success = bdd.DeleteMembre(id);
+                    }
+                    else if (dataGridView == competitionsDataGridView)
+                    {
+                        id = Convert.ToInt32(dataGridView["num_competition", e.RowIndex].Value);
+                        success = bdd.DeleteCompetition(id);
+                    }
+                    else
+                    {
+                        return; 
+                    }
+
+                    if (success)
+                    {
+                        dataGridView.Rows.RemoveAt(e.RowIndex);
+                    }
+                    else
+                    {
+                        MessageBox.Show("La suppression a échoué.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+
+
 
         private void NouveauMembreButton_Click(object sender, EventArgs e)
         {
